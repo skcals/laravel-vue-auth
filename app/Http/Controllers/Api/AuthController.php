cc411169ef6a840071b059b23d98a8394c495eff
Saurav Jain <saurav001024@gmail.com>
2022-03-02 23:17:08 +0530

    Add AuthContoller and Auth Api Routes

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+
+        $data = $request->validate([
+            'name'=>'required|string|max:255',
+            'email'=>'required|email|max:255|unique:users,email',
+            'password'=>'required|min:6|max:255|confirmed',
+        ]);
+
+        $user = User::create([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('lvvueauth')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request){
+
+        $data = $request->validate([
+            'email'=>'required|email|exists:users,email',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if(!$user || !Hash::check($data['password'], $user->password)){
+            return response([
+                'message'=>'Bad Creads'
+            ]);
+        }
+
+        $token = $user->createToken('lvvueapp')->plainTextToken;
+
+        return response([
+            'user'=>$user,
+            'token'=>$token
+        ]);
+    }
+
+    public function logout(Request $request){
+
+        auth()->user()->currentAccessToken()->delete(); 
+        
+        return response([
+            'success'=>true
+        ]);
+    }
+}

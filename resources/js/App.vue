eea7c4c977dfbf6e6aad48baabb61dde8c9d990d
Saurav Jain <saurav001024@gmail.com>
2022-03-03 00:28:05 +0530

    Setup Vue

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Hello From Vue Js</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'app'
+}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+
+createApp(App)
+.use(VueAxios, axios)
+.mount('#app');
+
